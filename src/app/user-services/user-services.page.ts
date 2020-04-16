87e3adbaa78d86bd86c4ba1785c2eccfe205fdbc
Niraj Pachpande <Niraj@Nirajs-MacBook-Pro.local>
2020-04-16 14:06:00 +0200

    Test Popup

--- src/app/user-services/user-services.page.ts
+++ src/app/user-services/user-services.page.ts
@@ -1,3 +1,4 @@
+import { ICoords } from './../models/shop.interface';
 import { GeolocationService } from './../services/geolocation/geolocation.service';
 import { Component, OnInit } from '@angular/core';
 import { NavController, AlertController } from '@ionic/angular';
@@ -11,6 +12,8 @@ import { Router } from '@angular/router';
 export class UserServicesPage implements OnInit {
 
   private userCoords: any = {};
+  lat: any;
+  long: any;
   constructor(public router: Router,
     public alertController: AlertController,
     private geolocation: GeolocationService) { }
@@ -18,6 +21,9 @@ export class UserServicesPage implements OnInit {
 
   ngOnInit() {
     this.checkUserPreference();
+    this.geolocation.userCoords.subscribe(data => {
+      this.presentAlertConfirm2();
+    });
 
   }
 
@@ -63,12 +69,44 @@ export class UserServicesPage implements OnInit {
     if (!localStorage.getItem('useCurrentLocation')) {
       this.presentAlertConfirm();
     }
-    this.geolocation.getCurrentLocation().then(data => {
+    this.geolocation.watchUserLocation().subscribe(data => {
       console.log('***: ', data);
+      this.lat = data['coords'].latitude;
+      this.long = data['coords'].longitude;
+      const _coords: ICoords = {
+        latitude: data['coords'].latitude,
+        longitude: data['coords'].longitude
+      }
+      this.geolocation.userCoords.next(_coords);
       this.userCoords['lat'] = data['coords'].latitude;
       this.userCoords['long'] = data['coords'].longitude;
 
     })
+  }
+
+  async presentAlertConfirm2() {
+    const alert = await this.alertController.create({
+      header: 'Save preference',
+      message: `<strong>Lat: ${this.lat} - Long: ${this.long}</strong>!!!`,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'danger',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+            localStorage.setItem('useCurrentLocation', 'true');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
